7174a4c85c6d184cf29727df2713830b6bede061
Camille <camille.meunier21@gmail.com>
2026-04-07 21:47:52 +0200

    Clean-up main.py

--- main.py
+++ main.py
@@ -7,40 +7,12 @@ from utils.logger import ensure_log_directory
 
 logger = __import__("utils.logger", fromlist=["logger"]).logger
 
-
 # Initialisation du logging
 ensure_log_directory()
 
-# # # Récupérer les prêts
-# api = MediathequeAPI()
-# loans = api.get_loans()
-# print(loans)
-
-loans = [
-    {
-        "title": "Internet et libertés : 15 ans de combat de la Quadrature du net / Mathieu Labonde, Lou Malhuret, Benoît Piedallu, Axel Simon",
-        "due_date": "2026-04-15",
-        "loan_number": "20022282",
-        "author": None,
-        "publisher": None,
-        "url": "https://cataloguebm.villeurbanne.fr/Default/doc/SYRACUSE/4377550/internet-et-libertes-15-ans-de-combat-de-la-quadrature-du-net-mathieu-labonde-lou-malhuret-benoit-pi",
-        "isbn": None,
-        "location": "Médiathèque du Rize",
-        "can_renew": True,
-    },
-    {
-        "title": "Neuromancien / William Gibson",
-        "due_date": "2026-04-15",
-        "loan_number": "20022283",
-        "author": None,
-        "publisher": None,
-        "url": "https://cataloguebm.villeurbanne.fr/Default/doc/SYRACUSE/4286445/neuromancien-william-gibson",
-        "isbn": None,
-        "location": "Médiathèque du Rize",
-        "can_renew": False,
-    },
-]
-
+# Récupérer les prêts
+api = MediathequeAPI()
+loans = api.get_loans()
 
 # Filtrer les livres dans le futur
 today = datetime.now().strftime("%Y-%m-%d")
